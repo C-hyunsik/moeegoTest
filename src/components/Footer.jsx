a559b452b4658cc6cdf95d8e430110678e27ce8c
gustlr887 <gustlr887@naver.com>
2024-11-18 14:04:42 +0900

    1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "../css/Footer.css";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
     <footer className="footer">
       <div className="ulWrap">
         <ul>모이고소개
-          <li><a href="">페이지소개</a></li>
+          <li><a href="/about">페이지소개</a></li>
         </ul>
         <ul>고객센터
           <li><a href="">챗봇상담</a></li>
---
+++ src/components/notice_eventpage/Notice_eventpage.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Notice_eventpage = () => {
+    return (
+        <div className='Notice_eventPage'>
+            <h2>공지사항 / 이벤트</h2>
+            <div className='noticeListWrap'>
+                <p>공지사항</p>
+                <ul>
+                    <li>
+                        map 돌려서 찍기
+                    </li>
+                </ul>
+            </div>
+            <div className='eventListWrap'>
+                <p>이벤트</p>
+                <ul>
+                    <li>
+                        map 돌려서 찍기
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Notice_eventpage;
